a09cdd28ad9cb0f2ddb8948dc99d15c8cc58d386
浩丶IMOYH <oyhemail@163.com>
2025-09-18 13:34:44 +0800

    refactor(modules): 移除 LogsModule 依赖
    - 从 ModulesModule 中删除了 LogsModule 的导入
    - 简化模块依赖关系，减少未使用模块代码
    - 优化整体模块结构，便于维护和扩展

--- src/modules/modules.module.ts
+++ src/modules/modules.module.ts
@@ -2,11 +2,10 @@ import { Module } from '@nestjs/common';
 
 import { AdminModule } from './admin/admin.module';
 import { CompanyModule } from './company/company.module';
-import { LogsModule } from './logs/logs.module';
 import { MediaModule } from './media/media.module';
 import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [AdminModule, UserModule, CompanyModule, MediaModule, LogsModule],
+  imports: [AdminModule, UserModule, CompanyModule, MediaModule],
 })
 export class ModulesModule {}
