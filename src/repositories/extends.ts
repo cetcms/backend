3e03c95ad3069db8833bb07cb944d43c2547221c
浩丶IMOYH <oyhemail@163.com>
2025-10-15 15:16:47 +0800

    feat(repositories): 导出认证扩展模块
    - 添加了认证扩展模块的导出
    - 更新了扩展模块的聚合导出文件

--- src/repositories/extends.ts
+++ src/repositories/extends.ts
@@ -1,4 +1,5 @@
 export * from './admin/admin.extend';
+export * from './auth/auth.extend';
 export * from './company/company.extend';
 export * from './user/user.extend';
 export * from './media-file/media-file.extend';
